e1894f8de1ff7b9c77123531bb49d97a9a79dc54
yermilov <yermilov17@gmail.com>
2018-04-17 00:09:56 +0300

    migrate from nb:0.1 to nb:0.2

--- src/main/groovy/me/potic/models/service/ModelService.groovy
+++ src/main/groovy/me/potic/models/service/ModelService.groovy
@@ -10,13 +10,13 @@ class ModelService {
 
     static final Model RANDOM = new Model(name: 'random', version: '1.0', description: 'random ranks', timestamp: '2017-12-13', isActive: true)
     static final Model LOGREG = new Model(name: 'logreg', version: '0.1', description: 'logistic regression (source, words count)', timestamp: '2018-03-01', isActive: true)
-    static final Model NB_0_1 = new Model(name: 'nb', version: '0.1', description: 'naive bayes (source, words count)', timestamp: '2018-04-15', isActive: true)
-    static final Model NB_0_2 = new Model(name: 'nb', version: '0.2', description: 'naive bayes (source, words count, showed count, skipped count)', timestamp: '2018-04-16', isActive: false)
+    static final Model NB_0_1 = new Model(name: 'nb', version: '0.1', description: 'naive bayes (source, words count)', timestamp: '2018-04-15', isActive: false)
+    static final Model NB_0_2 = new Model(name: 'nb', version: '0.2', description: 'naive bayes (source, words count, showed count, skipped count)', timestamp: '2018-04-16', isActive: true)
 
     static final List<Model> MODELS = [ RANDOM, LOGREG, NB_0_1, NB_0_2 ]
 
     Model getActualModel() {
-        NB_0_1
+        NB_0_2
     }
 
     List<Model> getActiveModels() {
