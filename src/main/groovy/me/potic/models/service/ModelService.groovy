fc119064d29f21c83d21b088749de54732da0e64
yermilov <yermilov17@gmail.com>
2018-04-16 03:14:14 +0300

    add 'nb:0.1' model

--- src/main/groovy/me/potic/models/service/ModelService.groovy
+++ src/main/groovy/me/potic/models/service/ModelService.groovy
@@ -10,12 +10,13 @@ class ModelService {
 
     static final Model RANDOM = new Model(name: 'random', version: '1.0', description: 'random ranks', timestamp: '2017-12-13T01:12:00.000', isActive: true)
     static final Model LOGREG = new Model(name: 'logreg', version: '0.1', description: 'logistic regression based on source and words count', timestamp: '2018-03-01T10:29:00.000', isActive: true)
+    static final Model NB = new Model(name: 'nb', version: '0.1', description: 'naive bayes based on source and words count', timestamp: '2018-04-16T03:12:00.000', isActive: true)
 
     Model getActualModel() {
         LOGREG
     }
 
     List<Model> getActiveModels() {
-        [ RANDOM, LOGREG ]
+        [ RANDOM, LOGREG, NB ]
     }
 }
